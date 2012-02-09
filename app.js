156af2274511c167ffe7da8b2b17ea21755e5e28
Mariusz Lusiak <mariusz@applicake.com>
2012-02-09 20:38:18 +0100

    Implements basic /geeks call

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/geeks', routes.geeks);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 /*
  * GET home page.
  */
+var GeeksFinder = require('../lib/geeks_finder');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Github Detector' })
 };
+
+exports.geeks = function(req, res){
+  var geeksFinder = new GeeksFinder();
+  res.send(geeksFinder.find());
+};
+
